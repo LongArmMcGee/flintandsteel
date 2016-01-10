821047393637b88b5eaeda83dd0ba58b9aa04160
LongArmMcGee <ncmmjw46@gmail.com>
2016-01-10 08:13:52 -0500

    idk what i'm doing
    saving old stuff

--- src/utilities/autoFocus.js
+++ src/utilities/autoFocus.js
@@ -1,15 +1,34 @@
 /* global angular */
 // This directive is used to bring focus to an input element when is becomes visible
 
+// angular.module('flintAndSteel')
+// .directive('autoFocus', function($timeout) {
+//     return {
+//         restrict: 'AC',
+//         link: function(_scope, _element) {
+//             $timeout(function(){
+//                 _element[0].focus();
+//                 console.log("test");
+//             }, 0);
+//         }
+//     };
+// });
+
 angular.module('flintAndSteel')
 .directive('autoFocus', function($timeout) {
     return {
         restrict: 'AC',
+        scope: {
+            updatesSelected: "=updatesSelected"
+        },
         link: function(_scope, _element) {
-            $timeout(function(){
-                _element[0].focus();
-                console.log("test");
-            }, 0);
+            _scope.$watch('_scope.updatesSelected',function(){
+                console.log("test watch");
+                if(_scope.updatesSelected || (typeof _scope.updatesSelected === 'undefined'){
+                    _element[0].focus();
+                    console.log("test if block");
+                }
+            }, 250)
         }
     };
 });
